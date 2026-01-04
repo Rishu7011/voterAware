eb45f93e1886a8ab5f44a247f33338b33a911dac
Rishabh Negi <negirishabh7011@gmail.com>
2026-01-04 11:54:50 +0530

    add report in main branch

---
+++ lib/report.ts
@@ -0,0 +1,36 @@
+const BASE_URL = "http://192.168.1.5:3000"
+
+export async function submitReport({
+  description,
+  reason,
+  image,
+}: {
+  description: string
+  reason: string
+  image?: any
+}) {
+  const formData = new FormData()
+
+  formData.append("description", description)
+  formData.append("reason", reason)
+
+  if (image) {
+    formData.append("image", {
+      uri: image.uri,
+      type: image.mimeType || "image/jpeg",
+      name: image.name || "report.jpg",
+    } as any)
+  }
+
+  const res = await fetch(`${BASE_URL}/report/submit`, {
+    method: "POST",
+    credentials: "include",
+    body: formData, 
+  })
+
+  if (!res.ok) {
+    throw new Error("Failed to submit report")
+  }
+
+  return res.json()
+}
